5712f437d152618ebe2991e2fe620fa7af9db30d
yasinooo777 <58569983+yasinooo777@users.noreply.github.com>
2020-02-29 14:56:10 -0500

    Update login.java

--- src/test/login.java
+++ src/test/login.java
@@ -3,5 +3,6 @@ package test;
 public class login {
     public static void main(String[] args) {
         System.out.println("user logged in");
+         System.out.println("from github");
     }
 }
